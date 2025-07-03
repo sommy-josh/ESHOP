45a2a347c4edf2be541914579a5e20a7add904eb
Anyiamjoel <chisomzzy1@gmail.com>
2025-07-03 10:33:22 +0100

    changed the allowed host settings

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG=False
 
-ALLOWED_HOSTS = ['eshop-2-e7ib.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['eshop-2-e7ib.onrender.com', '127.0.0.1','localhost']
 
 
 
