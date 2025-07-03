da911108d8a787cfddf456823b93a34374d5b0ee
Anyiamjoel <chisomzzy1@gmail.com>
2025-07-03 09:35:24 +0100

    changed the allowed host settings

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG=False
 
-ALLOWED_HOSTS = ['https://eshop-3-ss1a.onrender.com']
+ALLOWED_HOSTS = ['https://eshop-2-e7ib.onrender.com']
 
 
 # Application definition
